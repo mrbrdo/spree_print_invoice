783943d6f1cc92f13b05225b176e1615c5655499
mrbrdo <mrbrdo@gmail.com>
2024-04-22 15:47:40 +0200

    Prevent prepending OrderDecorator multiple times, which will cause problems (zeitwerk)

--- lib/spree_print_invoice/engine.rb
+++ lib/spree_print_invoice/engine.rb
@@ -19,7 +19,7 @@ module SpreePrintInvoice
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
+        require(c)
       end
     end
 
